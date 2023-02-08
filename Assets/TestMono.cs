8628119354cd4046e89ad97a9037b9c314fc8ae8
hallo.quantum <hallo@halloquantumdeMac-mini.local>
2023-02-08 18:35:33 +0800

    init commit

---
+++ Assets/TestMono.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMono : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("I am starting!");
+    }
+
+    void Update()
+    {
+        Debug.Log("I am updating!");
+    }
+}
